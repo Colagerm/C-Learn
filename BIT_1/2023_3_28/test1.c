afe5802d91a546689a8b98f06b13be95b5d1ece1
Colagerm <1586305005@qq.com>
2023-03-31 09:05:54 +0800

    做题

---
+++ BIT_1/2023_3_28/test1.c
@@ -0,0 +1,9 @@
+
+#include <stdio.h>
+
+int main() {
+    int a = 10, b = 20, max;
+    max = (a > b) ? a : b;
+    printf("Max value is %d\n", max);
+    return 0;
+}
---
+++ BIT_1/2023_3_28/test2.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int sum(int a)
+{
+    int c = 0;
+    static int b = 3;
+    c += 1;
+    b += 2;
+    return (a + b + c);
+}
+int main()
+{
+    int i;
+    int a = 2;
+    for (i = 0; i < 5; i++) 
+    { 
+        printf("%d,", sum(a)); 
+    } 
+} 
